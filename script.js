f291ae0c80de43fef4bf0b27f45eea80c34b8c35
Kieran Ridley <52752242+kieranridley95@users.noreply.github.com>
2025-09-22 20:14:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Toggle mobile menu
+// Mobile nav toggle
 document.getElementById('menu-toggle').addEventListener('click', () => {
   document.querySelector('nav ul').classList.toggle('show');
 });
 
-// Set current year in footer
+// Dynamic year
 document.getElementById('year').textContent = new Date().getFullYear();

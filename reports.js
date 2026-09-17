e451b6e69618cb49d6d962004c3e39de5bb96ff4
Kieran Ridley <52752242+kieranridley95@users.noreply.github.com>
2026-09-17 15:52:10 +0100

    Update reports.js

--- reports.js
+++ reports.js
@@ -1,6 +1,6 @@
 const reportMap = {
     reportvc: 'match-reports/velocity-cobras.txt',
-    reportwwc: 'match-reports/washington-wildcats.txt
+    reportwwc: 'match-reports/washington-wildcats.txt'
 
 };
 
